f2a513780b6d73dbf1173620b81e95276e23045e
McRhys189 <muragerhys189@gmail.com>
2020-05-17 12:48:36 +0300

    Avoiding duplicate names when signing in users

--- Section5/user.py
+++ Section5/user.py
@@ -55,11 +55,16 @@ class UserRegister(Resource):
         help = 'This field cannot be left blank'
     )
 
+    
     def post(self):
         data = UserRegister.parser.parse_args()
+        
+        if User.find_by_username(data['username']):
+            return {'message': 'A user with that username already exists'},400
 
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
+
 
         query = 'INSERT INTO users VALUES (NULL,?,?)'
         cursor.execute(query,(data['username'], data['password']))
